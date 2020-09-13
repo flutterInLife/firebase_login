27e7540db50f57a5974e2d2a9feb2183f7aaf214
TAMU73 <47109205+TAMU73@users.noreply.github.com>
2020-09-13 18:53:51 +0545

    Initial commit

---
+++ android/app/src/main/java/np/com/sanjivgrg/firebase_login/MainActivity.java
@@ -0,0 +1,6 @@
+package np.com.sanjivgrg.firebase_login;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Main(),
+  ));
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
